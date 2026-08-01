331cabee25bda3dedf0a90fbbf0e49dcbcd4eaf3
madhantr0 <madhantr0@gmail.com>
2026-08-01 11:11:59 +0530

    Delete frontend directory

--- frontend/config.js
+++
@@ -1,6 +0,0 @@
-// Point this at your deployed backend URL.
-// Local development (backend running on your machine):
-window.SCANGRADE_API_BASE = 'http://localhost:3000';
-
-// Once your backend is deployed on Render, replace the line above with, e.g.:
-// window.SCANGRADE_API_BASE = 'https://scangrade-api.onrender.com';
